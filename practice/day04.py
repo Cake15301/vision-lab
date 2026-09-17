60c3d02e87df269f9a9fde5a01562c7fca46d967
lzj <770312053@qq.com>
2026-09-17 20:40:45 +0800

    feat: Python Day4 条件与循环

---
+++ practice/day04.py
@@ -0,0 +1,13 @@
+n=8
+print(n % 2)
+print(n % 2 == 0)
+if n % 2 == 0:
+    print("n是偶数")
+else:
+    print("n是奇数")
+print(list(range(1,11)))
+for n in range(1,11):
+    if n % 2 == 0:
+        print(f"{n}是偶数")
+    else:
+        print(f"{n}是奇数")
